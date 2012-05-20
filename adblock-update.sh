8a3bf6e7da2e0731cbde66a9db7ec0d0288301f0
maximilian_dietrich@t-online.de <doom@sid>
2012-05-20 20:04:14 +0200

    fixed some stuff

--- adblock-update.sh
+++ adblock-update.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
+[[ -z "$XDG_DATA_HOME" ]] && DATADIR="$HOME/.local/share" || DATADIR="XDG_DATA_HOME"
+
 # look for adblock directory or fallback
-[[ -d ~/.local/share/luakit/adblock/ ]] && cd ~/.local/share/luakit/adblock/ || cd ~/.local/share/luakit/
+[[ -d $DATADIR/luakit/adblock/ ]] && cd $DATADIR/luakit/adblock/ || cd $DATADIR/luakit/
 
 # backup the old list
-[[ -f easylist.txt ]] && cp easylist.txt easylist.txt.b
+[[ -f easylist.txt ]] && cp -p easylist.txt easylist.txt.b
 
-wget -N --connect-timeout=10 --retry-connrefused --wait=5 https://easylist-downloads.adblockplus.org/easylist.txt
+wget -N --connect-timeout=10 --tries=20 --retry-connrefused --waitretry=5 https://easylist-downloads.adblockplus.org/easylist.txt
 
 # if download failed move old file back in place
-if [[ $? != 0 ]]; then
+if (( $? != 0 )); then
 	[[ -f easylist.txt.b ]] && mv easylist.txt.b easylist.txt
 	echo "Error: Easylist Download Failed!"
-	exit 1
+	exit 11
 else
 	[[ -f easylist.txt.b ]] && rm easylist.txt.b # if all went well remove backup
 	echo "All went well. :)"

47f68e2a94724f3b132732c16e671a187e1eb55a
maximilian_dietrich@t-online.de <doom@sid>
2012-05-20 17:06:58 +0200

    Pulls only when update on server + backup old easylist

--- adblock-update.sh
+++ adblock-update.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
+# look for adblock directory or fallback
+[[ -d ~/.local/share/luakit/adblock/ ]] && cd ~/.local/share/luakit/adblock/ || cd ~/.local/share/luakit/
 
-[[ -d /tmp ]] && cd /tmp/
+# backup the old list
+[[ -f easylist.txt ]] && cp easylist.txt easylist.txt.b
 
-rm ./easylist.txt*
+wget -N --connect-timeout=10 --retry-connrefused --wait=5 https://easylist-downloads.adblockplus.org/easylist.txt
 
-while ! wget https://easylist-downloads.adblockplus.org/easylist.txt; do
-    sleep 0.2s;
-done;
-
-if [[ -d ~/.local/share/luakit/adblock/ ]]; then
-    mv ./easylist.txt ~/.local/share/luakit/adblock/;
+# if download failed move old file back in place
+if [[ $? != 0 ]]; then
+	[[ -f easylist.txt.b ]] && mv easylist.txt.b easylist.txt
+	echo "Error: Easylist Download Failed!"
+	exit 1
 else
-    mv ./easylist.txt ~/.local/share/luakit/;
-fi;
-
-exit
+	[[ -f easylist.txt.b ]] && rm easylist.txt.b # if all went well remove backup
+	echo "All went well. :)"
+	exit 0
+fi

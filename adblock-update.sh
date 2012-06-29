6ab1a9293ecce78f273abadd83800f18f2d2f2e9
Doomicide <maximilian_dietrich@t-online.de>
2012-06-29 23:19:13 +0200

    use a more general approach

--- adblock-update.sh
+++ adblock-update.sh
@@ -1,22 +1,55 @@
 #!/bin/bash
+#
+# Usage: adblock-update.sh [-h] [URL]
+# This script can be used to download/update filterlists for the luakit adblock-module
+# It will only update the file if the server-side version is newer.
+#
+#	URL		URL of the filterlist to download, leave empty to download Easylist
+#	-h, --help	Display this help-message and exit
 
+# help message
+function usage ()
+{
+cat << EOF
+Usage: $0 [-h] [URL]
+This script can be used to download/update filterlists for the luakit adblock-module.
+It will only update the file if the server-side version is newer.
+
+	URL		URL of the filterlist to download, leave empty to download Easylist
+	-h, --help	Display this help message and exit
+EOF
+}
+
+# check for $XDG_DATA_HOME or fallback
 [[ -z "$XDG_DATA_HOME" ]] && DATADIR="$HOME/.local/share" || DATADIR="$XDG_DATA_HOME"
 
-# look for adblock directory or fallback
-[[ -d "$DATADIR/luakit/adblock/" ]] && cd "$DATADIR/luakit/adblock/" || cd "$DATADIR/luakit/"
+
+# use URL if given else default to easylist.txt
+if (( $# == 1 )) || (( $# == 0 )); then
+	[[ ! -z $1 ]] && [[ $1 == "-h" ]] || [[ $1 == "--help" ]] && usage && exit 0  # check for -hflag
+	listurl="${1-"https://easylist-downloads.adblockplus.org/easylist.txt"}"
+	listname="$(basename ${listurl})"
+elif (( $# > 1 ));then
+	usage
+	exit 11
+fi
+
+# look for adblock directory || create
+[[ -d "$DATADIR/luakit/adblock/" ]] && cd "$DATADIR/luakit/adblock/" || mkdir "$DATADIR/luakit/adblock/" && cd "$DATADIR/luakit/adblock/"
 
 # backup the old list
-[[ -f easylist.txt ]] && cp -p easylist.txt easylist.txt.b
+[[ -f ${listname} ]] && cp -p ${listname} ${listname}.b
 
-wget -N --connect-timeout=10 --tries=20 --retry-connrefused --waitretry=5 https://easylist-downloads.adblockplus.org/easylist.txt
+# try to download the file
+wget -N --connect-timeout=10 --tries=20 --retry-connrefused --waitretry=5 ${listurl}
 
 # if download failed move old file back in place
 if (( $? != 0 )); then
-	[[ -f easylist.txt.b ]] && mv easylist.txt.b easylist.txt
-	echo "Error: Easylist Download Failed!"
+	[[ -f ${listname}.b ]] && cp -p ${listname}.b ${listname} && rm ${listname}.b
+	echo "Error: List Download Failed!"
 	exit 11
 else
-	[[ -f easylist.txt.b ]] && rm easylist.txt.b # if all went well remove backup
+	[[ -f ${listname}.b ]] && rm ${listname}.b # if all went well remove backup
 	echo "All went well. :)"
 	exit 0
 fi
